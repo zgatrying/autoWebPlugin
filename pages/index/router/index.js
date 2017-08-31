1c83dda07902e1117533dd8dae7694269b9d0a8b
zgatrying <279501822@qq.com>
2017-08-31 11:24:27 +0800

    禁用路由

--- pages/index/router/index.js
+++ pages/index/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Menu from '../components/Menu'
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Menu from '../components/Menu'
 
-Vue.use(Router)
+// Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Menu',
-      component: Menu
-    }
-  ]
-})
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Menu',
+//       component: Menu
+//     }
+//   ]
+// })
