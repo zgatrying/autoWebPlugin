ce593f2626a16bbea93de9fe18149e9e8a2c8fc1
zgatrying <279501822@qq.com>
2017-09-01 11:22:16 +0800

    [add] 引入MD5依赖

--- pages/list/index.js
+++ pages/list/index.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-// import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
@@ -12,7 +11,6 @@ Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
   template: '<App/>',
   components: { App }
 })
